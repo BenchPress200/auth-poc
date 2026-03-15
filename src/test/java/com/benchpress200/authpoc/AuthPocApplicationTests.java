b6e69bde7ecd8461c6fed13cbaf886f0be605a0e
BenchPress200 <jms0538@naver.com>
2026-03-16 00:28:33 +0900

    init

---
+++ src/test/java/com/benchpress200/authpoc/AuthPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchpress200.authpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
